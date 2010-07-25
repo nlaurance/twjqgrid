0462e15af3a29525b767cb300f8f059e7efbf735
nicolas.laurance@gmail.com <nicolas.laurance@gmail.com@288325f2-5fb3-25e8-dae7-904ad34f8554>
2010-07-25 19:21:52 +0000

    untested implementation for backup purpose

--- trunk/tw/jqgrid/widgets.py
+++ trunk/tw/jqgrid/widgets.py
@@ -1,35 +1,80 @@
 from tw.api import Widget, JSLink, CSSLink
+from tw.jquery import jquery_js
+from tw.jquery.ui import jquery_ui_all_js
+from tw.jquery.ui import
 
 __all__ = ["Jqgrid"]
 
 # declare your static resources here
+i18n_jqgrid = JSLink(modname=__name__,
+               filename='static/i18n/grid.locale-en.js',
+               javascript=[])
 
-## JS dependencies can be listed at 'javascript' so they'll get included
-## before
-# my_js = JSLink(modname=__name__, 
-#                filename='static/jqgrid.js', javascript=[])
+jquery_jqgrid = JSLink(modname=__name__,
+               filename='static/javascript/jquery.jqGrid.min.js',
+               javascript=[jquery_js, jquery_ui_all_js, i18n_jqgrid])
 
-# my_css = CSSLink(modname=__name__, filename='static/jqgrid.css')
+jqgrid_ui_css = CSSLink(modname=__name__, filename='static/css/jquery-ui-1.8.2.custom.css')
+jqgrid_css = CSSLink(modname=__name__, filename='static/css/ui.jqgrid.css')
 
-class Jqgrid(Widget):
-    template = """<div id="${id}">${value}</div>"""
-    ## You can also define the template in a separate package and refer to it
-    ## using Buffet style uris
-    #template = "tw.jqgrid.templates.jqgrid"
+class JqGrid(Widget):
+    """
+    jqgrid full options : http://www.trirand.com/jqgridwiki/doku.php?id=wiki:options
+    """
+    template = """
+             <table id="${id}"></table>
+             <div id="${id}_pager"></div>
+             """
 
-    #javascript = [my_js]
-    #css = [my_css]
+    javascript = [jquery_jqgrid]
+    css = [jqgrid_ui_css, jqgrid_css]
 
-    def __init__(self, id=None, parent=None, children=[], **kw):
-        """Initialize the widget here. The widget's initial state shall be
-        determined solely by the arguments passed to this function; two
-        widgets initialized with the same args. should behave in *exactly* the
-        same way. You should *not* rely on any external source to determine
-        initial state."""
-        super(Jqgrid, self).__init__(id, parent, children, **kw)
+    params = {
+           "url": "Tells us where to get the data.",
+           "datatype" : "This tells jqGrid the type of information being returned so it can construct the grid.",
+           "mtype" :"Tells us how to make the ajax call: either 'GET' or 'POST'.",
+           "colNames" :"An array in which we place the names of the columns. ",
+           # column model options : http://www.trirand.com/jqgridwiki/doku.php?id=wiki:colmodel_options
+           "colModel" :"An array that describes the model of the columns.",
+           "pager" :"Defines that we want to use a pager bar to navigate through the records.",
+           "rowNum" :"Sets how many records we want to view in the grid.",
+           "rowList" :"An array to construct a select box element in the pager in which we can change the number of the visible rows.",
+           "sortname" :"Sets the initial sorting column.",
+           "viewrecords" :"Defines whether we want to display the number of total records from the query in the pager bar",
+           "caption" :"Sets the caption for the grid.",
+           }
+
+    datatype = "json"
+    mtype = "GET"
+
+    def __init__(self, **kwargs):
+        """
+        """
+        super(JqGrid, self).__init__(**kwargs)
+        if not getattr(d,"id",None):
+            raise ValueError, "JqGrid is supposed to have id"
+        if not getattr(d,"url",None):
+            raise ValueError, "JqGrid must have url for fetching data"
+        if not getattr(d,"colModel",None):
+            raise ValueError, "JqGrid must have colModel for setting up the columns"
+
+        if not getattr(d,"pager",None):
+            self.pager = '%s_pager' % self.id
 
     def update_params(self, d):
-        """This method is called every time the widget is displayed. It's task
-        is to prepare all variables that are sent to the template. Those
-        variables can accessed as attributes of d."""
-        super(Jqgrid, self).update_params(d)
+        super(JqGrid, self).update_params(d)
+
+        grid_params = dict(url=self.url,
+                           datatype=self.datatype,
+                           mtype=self.mtype,
+                           colNames=self.colNames,
+                           colModel=self.colModel,
+                           pager=self.pager,
+                           rowNum=self.rowNum,
+                           rowList=self.rowList,
+                           sortname=self.sortname,
+                           viewrecords=self.viewrecords,
+                           caption=self.caption,
+                           )
+        call = js_function('$("#%s").jqGrid' % d.id)(grid_params)
+        self.add_call(call)
